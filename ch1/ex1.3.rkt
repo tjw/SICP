2a5421d2d409ef2ec6ef9115c26ca42a3fc1e12f
Timothy J. Wood <tjw@omnigroup.com>
2015-01-20 08:19:04 -0800

    Use sum-of-squares helper.

--- ch1/ex1.3.rkt
+++ ch1/ex1.3.rkt
@@ -1,10 +1,13 @@
 #lang racket
 
 ;; lame, but in keeping with what we know so far...
+(define (sum-of-squares a b)
+  (+ (* a a) (* b b)))
+
 (define (sum-of-squares-of-largest-two a b c)
-  (cond ((and (<= a b) (<= a c)) (+ (* b b) (* c c)))
-        ((and (<= b a) (<= b c)) (+ (* a a) (* c c)))
-        ((and (<= c a) (<= c b)) (+ (* a a) (* b b)))))
+  (cond ((and (<= a b) (<= a c)) (sum-of-squares b c))
+        ((and (<= b a) (<= b c)) (sum-of-squares a c))
+        ((and (<= c a) (<= c b)) (sum-of-squares a b))))
 
 (sum-of-squares-of-largest-two 2 3 4)
 (sum-of-squares-of-largest-two 4 2 3)
