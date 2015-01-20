2dae4d38701906cbe197c32c6e7a06b61046683f
Timothy J. Wood <tjw@omnigroup.com>
2015-01-20 13:09:00 -0800

    Capture the limit instead of passing it around.

--- ch1/ex1.11.rkt
+++ ch1/ex1.11.rkt
@@ -10,17 +10,17 @@
 ;; Iterative version
 (define (f-iter n)
   ; takes the current i, a limit, and f(i-1), f(i-2), and f(i-3) values, returns f(i)
-  (define (iter i limit fi1 fi2 fi3)
+  (define (iter i fi1 fi2 fi3)
     (define fi (+ fi1 (* 2 fi2) (* 3 fi3))) ; we'll use the value of f(n) no matter what
-    (if (= i limit)
+    (if (= i n)
         fi ; done
         
         ; substituting i for i+1: f(i+1) = f(i) + 2f(i-1) + 3f(i-2)
-        (iter (+ 1 i) limit fi fi1 fi2)))
+        (iter (+ 1 i) fi fi1 fi2)))
     
   (cond ((< n 3) n)
         ;; start iteration at 3 and pass in f(n-1), f(n-2), f(n-3)
-        (else (iter 3 n 2 1 0))))
+        (else (iter 3 2 1 0))))
 
 (f-recur 0)
 (f-recur 1)
