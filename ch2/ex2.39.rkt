9379b964eac65ba188ef663f53d1fe6998716046
Timothy J. Wood <tjw@omnigroup.com>
2015-02-23 21:55:52 -0800

    More exercises.

---
+++ ch2/ex2.39.rkt
@@ -0,0 +1,31 @@
+#lang racket
+
+(define (fold-right op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (fold-right op 
+                      initial 
+                      (cdr sequence)))))
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+;;;;
+
+(define (reverse-r sequence)
+  (fold-right 
+   (lambda (x y) (append y (list x))) null sequence))
+
+(define (reverse-l sequence)
+  (fold-left 
+   (lambda (x y) (cons y x)) null sequence))
+
+(reverse-r '(1 2 3))
+(reverse-l '(1 2 3))
+
