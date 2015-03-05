79ebac0e8b03a8b10447c75d99bd8b952232427a
Timothy J. Wood <tjw@omnigroup.com>
2015-03-04 20:11:53 -0800

    Added exponentation differentiation.

--- ch2/ex2.56.rkt
+++ ch2/ex2.56.rkt
@@ -34,7 +34,10 @@
         (else (list '* m1 m2))))
 
 (define (make-exponentiation base power)
-  (list '** base power))
+  (cond ((=number? power 0) 1)
+        ((=number? power 1) base)
+        (else (list '** base power))))
+
 (define (exponentiation? x)
   (and (pair? x) (eq? (car x) '**)))
 (define (base exp)
@@ -48,20 +51,36 @@
 (define (multiplicand p) (caddr p))
 
 (define (deriv exp var)
-  (cond ((number? exp) 0)
-        ((variable? exp)
-         (if (same-variable? exp var) 1 0))
-        ((sum? exp)
-         (make-sum (deriv (addend exp) var)
-                   (deriv (augend exp) var)))
-        ((product? exp)
-         (make-sum
+  (define (deriv-number x)
+    0)
+
+  (define (deriv-variable x)
+    (if (same-variable? exp var) 1 0))
+  
+  (define (deriv-sum x)
+    (make-sum (deriv (addend exp) var)
+              (deriv (augend exp) var)))
+    
+  (define (deriv-product x)
+    (make-sum
           (make-product 
            (multiplier exp)
            (deriv (multiplicand exp) var))
           (make-product 
            (deriv (multiplier exp) var)
            (multiplicand exp))))
-        (else (error "unknown expression type: DERIV" exp))))
+  
+  (define (deriv-exponentiation exp)
+    (make-product (make-product (exponent exp)
+                                (make-exponentiation (base exp) (- (exponent exp) 1)))
+                  (deriv (base exp) var)))
+                  
+  (let ((deriv-op (cond ((number? exp) deriv-number)
+                        ((variable? exp) deriv-variable)
+                        ((sum? exp) deriv-sum)
+                        ((product? exp) deriv-product)
+                        ((exponentiation? exp) deriv-exponentiation)
+                        (else (error "unknown expression type: DERIV" exp)))))
+    (deriv-op exp)))
 
-(deriv '(+ x 3) 'x)
+(deriv '(** (** x 2) 2) 'x)
