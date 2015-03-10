63aa0657ca3fd991fcf19fff795e430b21555f93
Timothy J. Wood <tjw@omnigroup.com>
2015-03-10 13:11:33 -0700

    Extra lets.

--- ch2/ex2.62.rkt
+++ ch2/ex2.62.rkt
@@ -5,11 +5,13 @@
 (define (union-set set1 set2)
   (cond ((null? set1) set2)
         ((null? set2) set1)
-        (else (let ((x1 (car set1))
-                    (x2 (car set2)))
-                (cond ((= x1 x2) (cons x1 (union-set (cdr set1) (cdr set2))))
-                      ((< x1 x2) (cons x1 (union-set (cdr set1) set2)))
-                      (else (cons x2 (union-set set1 (cdr set2)))))))))
+        (else (let ((h1 (car set1))
+                    (h2 (car set2))
+                    (t1 (cdr set1))
+                    (t2 (cdr set2)))
+                (cond ((= h1 h2) (cons h1 (union-set t1 t2)))
+                      ((< h1 h2) (cons h1 (union-set t1 set2)))
+                      (else (cons h2 (union-set set1 t2))))))))
 
 
 (union-set (list 1) (list 2))
