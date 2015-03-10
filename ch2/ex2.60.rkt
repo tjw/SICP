43e20c94c60f9a72b738c5d10653bbb8cacac914
Timothy J. Wood <tjw@omnigroup.com>
2015-03-09 21:17:43 -0700

    Ex.

---
+++ ch2/ex2.60.rkt
@@ -0,0 +1,5 @@
+#lang racket
+
+; seems boring -- just gets slower as duplicates pile up.
+; intersection-set is maybe interesting -- if you have (2 2 3) intersected with (2 4), do you get back (2) or (2 2)?
+
---
+++ ch2/ex2.61.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+; ordered list as set
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (car set)) true)
+        ((< x (car set)) false)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((= x (car set)) set)
+        ((< x (car set)) (cons x set))
+        (else (cons (car set) (adjoin-set x (cdr set))))))
+
+(adjoin-set 2 (list 1 2 3))
+(adjoin-set 4 (list 1 2 3))
+(adjoin-set 1 (list 2 3))
