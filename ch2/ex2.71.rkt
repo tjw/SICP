7a9e6f9558046df07bcc8c2b04cc8d6c1f37a55a
Timothy J. Wood <tjw@omnigroup.com>
2015-03-11 21:43:56 -0700

    Ex.

---
+++ ch2/ex2.71.rkt
@@ -0,0 +1,4 @@
+#lang racket
+
+; seems like it'll be an tree where each branching is a leaf and then the rest (1+2 = 3 < 4, 1+2+4 < 8, etc)
+; least 1, most n.
---
+++ ch2/ex2.72.rkt
@@ -0,0 +1,4 @@
+#lang racket
+
+; order of growth in number of steps needed to encode symbol from ex 2.68
+; depends on depth of tree, between O(log n) and O(n)
